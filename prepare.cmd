02af1dc2771f840b608bd2ed5cd708767ea30526
ryn <29420409+rynho@users.noreply.github.com>
2021-11-04 09:28:43 -0400

    Adding working folder check.

--- prepare.cmd
+++ prepare.cmd
@@ -1,10 +1,20 @@
 rem I made an equivalent of prepare.sh for Windows user, therefore not necessary for using WSL.
+@echo off
+cd data
+
+if not %cd:~-4% == data goto error
 
 del *.dll /f/s/q
 del *.exe /f/s/q
 del SorMaker.dat /f/s/q
 
-cd data
 ..\tools\extract_stub.py SorR.dat
 move /y SorR_stripped.dcb SorR.dat
 copy /y ..\system.txt mod\system.txt
+
+goto end
+
+:error
+echo Error: You need to run this in the parent folder of "data".
+
+:end
